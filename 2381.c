7777e3d4779bb9dc467168ea09e72b09586cde8c
migueldmo <89785768+migueldmo@users.noreply.github.com>
2023-04-28 22:50:08 -0300

    Add files via upload

---
+++ 2381.c
@@ -0,0 +1,108 @@
+/*Problema 2381 Beecrowd
+Autor: Miguel de Moura Oliveira
+Disciplina: Algorítmos e Estrutura de Dados 1*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX 100
+
+typedef struct elemento
+{
+    char nome[MAX];
+    struct elemento *prox; 
+} Tipoelemento;
+
+typedef struct Lista
+{
+    Tipoelemento *primeiro, *ultimo;
+    int t;
+}TipoLista;
+
+/*O procedimento abaixo recebe uma lista e ajusta seus parâmetros para os iniciais*/
+void inicializa(TipoLista *aux){
+    aux->primeiro = NULL;
+    aux->ultimo = NULL;
+    aux->t = 0;
+}
+
+/*O procedimento abaixo recebe um elemento a ser inserido em ordem alfabética e sua respectiva lista*/
+void insere_ordenado(TipoLista *aux, char *x){
+    Tipoelemento *novo, *p, *q;
+    novo = malloc(sizeof(Tipoelemento));
+
+    strcpy(novo->nome, x);
+
+    p = aux->primeiro;
+
+    if(aux->t == 0){
+        aux->primeiro = novo;
+        aux->ultimo = novo;
+        novo->prox = NULL;
+    }
+    else{
+        q = p;
+
+        if(strcmp(p->nome, x) > 0){
+            novo->prox = p;
+            aux->primeiro = novo;
+        }
+        else{
+            while(p != NULL && !(strcmp(p->nome,x) >= 0)){ 
+                q = p;
+                p = p->prox;
+            }   
+            if(q->prox != p)
+                q = q->prox;    
+            q->prox = novo;
+            novo->prox = p;
+            
+        }
+    }  
+
+    aux->t++;
+}
+/*O procedimento abaixo recebe uma lista e imprime seus elementos*/
+void imprime(TipoLista *aux){
+    Tipoelemento *p = aux->primeiro;
+
+    while(p != NULL){       
+        printf("%s ", p->nome);
+        p = p->prox;
+    }
+
+}
+/*A função abaixo recebe a lista e um inteiro x que representa o número da posição do elemento da lista a ser buscado
+e retorna o conteúdo deste elemento
+*/
+char* busca(TipoLista *aux, int x){
+    Tipoelemento *p;
+    int i = 1;
+
+    p = aux->primeiro;
+
+    while(i < x){
+        p = p->prox;
+        i++;
+    }
+    return p->nome;
+}
+
+int main(){
+    TipoLista Minhalista;
+    char nome[MAX];
+    int n,i, num;
+
+    inicializa(&Minhalista);
+
+    scanf("%d %d", &n, &num);
+
+    for(i = 0; i < n; i++){
+        scanf("%s", &nome);
+        insere_ordenado(&Minhalista, nome);
+    }
+
+    printf("%s\n", busca(&Minhalista, num));
+
+    return 0;
+}

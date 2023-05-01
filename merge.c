785f38d2a8de3bf344b51298a350b0edd802f194
migueldmo <89785768+migueldmo@users.noreply.github.com>
2023-05-01 18:07:55 -0300

    Update merge.c

--- merge.c
+++ merge.c
@@ -1,8 +1,14 @@
+/*Autor: Miguel de Moura Oliveira
+EP05 - AED1
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define TAM 10000
+#define TAM 5000 /*tamanho do vetor desejado (n). Para testar com outros parâmetros, basta alterar o valor desta variável global*/
 
+/*O procedimento abaixo recebe um vetor v[p,...,q-1] e v[q,...r-1] e 
+rearranja v[p,...,r-1] em ordem crescente.
+*/ 
 void intercala(int p, int q,int r, int v[]){
     int i, j, k, *w;
 
@@ -20,7 +26,7 @@ void intercala(int p, int q,int r, int v[]){
 
     free(w);
 }
-
+/*O procedimento abaixo rearranja o vetor v[p,...,r-1] em ordem crescente.*/
 void Mergesort(int p, int r, int v[]){
 
     if(p < r-1){
@@ -39,11 +45,11 @@ int main(){
     double total;
 
     srand(time(NULL));
-
+    /*Gerando um vetor com valores aleatórios*/
     for(i= 0; i < TAM; i++){
         v[i] = rand() % 10000;
     }
-    
+
     antes = clock();
     Mergesort(0,TAM-1, v);
     depois = clock();

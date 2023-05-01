fdc144456d6cf8795345259f9fcb1cd543968dbe
migueldmo <89785768+migueldmo@users.noreply.github.com>
2023-05-01 18:03:39 -0300

    Update selection.c

--- selection.c
+++ selection.c
@@ -1,10 +1,16 @@
+/*Autor: Miguel de Moura Oliveira
+EP05 - AED1
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define TAM 5000
+#define TAM 5000  /*tamanho do vetor desejado (n). Para testar com outros parâmetros, basta alterar o valor desta variável global*/
 
+/*O procedimento abaixo recebe um inteiro n e um vetor v[0,...n-1] e o 
+rearranja em ordem crescente.*/
 void selection(int n, int v[]){
     int i, j, min, x;
+
     for(i = 0; i < n-1; i++){
         min = i;
         for(j = i+1; j < n; j++)
@@ -20,13 +26,12 @@ int main(){
     long antes, depois;
     double total;
 
-    
-
     srand(time(NULL));
 
     for(i= 0; i < TAM; i++){
         v[i] = rand() % 10000;
     }
+    /*Gerando um vetor com valores aleatórios*/
     antes = clock();
     selection(TAM-1, v);
     depois = clock();

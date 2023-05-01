bc59ef3b042a37e13be7bb2c63218d35e2cf9fdc
migueldmo <89785768+migueldmo@users.noreply.github.com>
2023-05-01 18:11:50 -0300

    Add files via upload

---
+++ heap.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define TAM 20000
+
+void insereEmHeap(int m, int v[]){
+    int t, f = m+1;
+
+    while(f > 1 && v[f/2] < v[f]){
+        t = v[f/2];
+        v[f/2] = v[f];
+        v[f] = t;
+        f = f/2;
+    }
+}
+
+void heapify(int m, int v[]){
+    int t, f = 2;
+
+    while(f <= m){
+        if(f < m && v[f] < v[f+1])  ++f;
+        if(v[f/2] >= v[f])  break;
+        t = v[f/2]; v[f/2] = v[f]; v[f] = t;
+        f *= 2;
+    }
+}
+
+void Heapsort(int n, int v[]){
+    int m, t;
+
+    for(m = 1; m < n; m++)
+        insereEmHeap(m, v);
+
+    for(m = n; m > 1; m--){
+        t = v[1]; v[1] = v[m]; v[m] = t;
+        heapify(m-1, v);
+    }
+}
+
+void imprime(int m ,int v[]){
+    int i;
+
+    for(i = 0; i < m; i++)
+        printf("%d ", v[i]);
+}
+
+int main(){
+    int v[TAM];
+    int i;
+    long antes, depois;
+    double total;
+
+    srand(time(NULL));
+
+    for(i= 0; i < TAM; i++){
+        v[i] = rand() % 10000;
+    }
+
+    antes = clock();
+    Heapsort(TAM-1, v);
+    depois = clock();
+
+    total = (double)(depois-antes)/CLOCKS_PER_SEC;
+
+    printf("%lf", total);
+
+    //imprime(TAM, v);
+
+}
